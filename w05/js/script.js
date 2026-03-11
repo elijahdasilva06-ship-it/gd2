745e4eb1eb9b200262265fbcb55f09da4ef3b381
Elijah <elijahdasilva06@gmail.com>
2026-03-11 16:49:48 -0400

    g

--- w05/js/script.js
+++ w05/js/script.js
@@ -1,3 +1,16 @@
 $("#preamble-btn-1").click(function(){
-    $("#preamble-btn-2").remove;
+    $("#preamble-btn-2").css({
+        "background":"blue",
+        "transform":"rotate(45deg)"
+        
+    })
 })
+
+$("#preamble-btn-2").click(function(){
+    $("#preamble-btn-1").toggleclass("rotate")
+        
+    })
+
+$("#h1").click(function(){
+
+    ("#menu").toggleClass("active")
