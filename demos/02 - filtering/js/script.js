3b04165bb52b9a7b987fcdfc8e0779fe214d1d6c
Elijah <elijahdasilva06@gmail.com>
2026-03-18 00:30:00 -0400

    d

--- demos/02 - filtering/js/script.js
+++ demos/02 - filtering/js/script.js
@@ -2,3 +2,18 @@
 //map libre
 //add custom marker
 //add cusgtom popup
+
+$(".filters button").click(function() {
+    
+
+
+    f = $(this).data("year");
+
+    $(f).fadeToggle(500);
+
+    
+    //get the year that is assosciated with the button
+    //find an element that has that year
+    //hide that element
+
+})
