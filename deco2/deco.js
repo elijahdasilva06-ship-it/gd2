8d7b72d35fc089e73703c7d8396de116a13c5143
Elijah <elijahdasilva06@gmail.com>
2026-04-13 06:17:16 -0400

    b

--- deco2/deco.js
+++ deco2/deco.js
@@ -5,12 +5,9 @@ $(function() {
   });
 
   $(".draggable").mousedown(function() {
-    $(".draggable").css("z-index", 1);
+    $(".draggable").css("z-index", 10);
     $(this).css("z-index", 10);
   });
 });
 
 
-items.forEach(el => {
-  el.style.opacity = hidden ? "0" : "1";
-});
